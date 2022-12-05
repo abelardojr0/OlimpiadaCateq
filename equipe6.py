61aa17d54b1405d1ab38396af1a1e134766f01fc
Abelardo Júnior <abelardo.junior.181@gmail.com>
2022-12-05 16:59:33 -0300

    att

--- equipe6.py
+++ equipe6.py
@@ -1,7 +1,14 @@
+def separarEquipes(lista, n):
+    for i in range(0, len(lista), n):
+        yield lista[i:i + n]
+
+
 listaAlunos = []
 qtdeAlunos = int(input("Digite a quantidade de alunos: "))
 qtdeEquipe = int(input("Digite a quantidade de equipes: "))
 for aluno in range(qtdeAlunos):
     aluno = input("Digite o nome do aluno: ")
     listaAlunos.append(aluno)
-print(listaAlunos)
+alunosPorEquipe = int(qtdeAlunos / qtdeEquipe)
+
+print(list(separarEquipes(listaAlunos, alunosPorEquipe)))
