9640648d4153954e4f4135e978b1c99c63c6be71
Abelardo Júnior <abelardo.junior.181@gmail.com>
2022-12-05 17:01:05 -0300

    att: return romanos

--- Equipe3 - Solução Romanos.py
+++ Equipe3 - Solução Romanos.py
@@ -8,30 +8,33 @@ romanos = {
     "M": 1000,
 }
 soma = 0
+
+
 def transformar(romano):
-    
+
     arrayRomano = []
     arrayRomano[:0] = romano
-    
+
     if romanos[arrayRomano[-1]] > romanos[arrayRomano[-2]]:
         global soma
-        soma =  romanos[arrayRomano[-1]] -  romanos[arrayRomano[-2]]
+        soma = romanos[arrayRomano[-1]] - romanos[arrayRomano[-2]]
         del arrayRomano[-1]
         del arrayRomano[-1]
     for i in arrayRomano:
         soma += romanos[i]
-    print(soma)
-    
+    return soma
+
+
 num = input("Digite o número romano: ")
 arrayTeste = ["I", "V", "X", "L", "C", "D", "M"]
 
 cont = 0
 for letra in num:
     if letra.upper() in arrayTeste:
-        cont+=1
+        cont += 1
 
 
 if cont == len(num):
-    transformar(num.upper())
+    print(transformar(num.upper()))
 else:
     print("Você não digitou um número romano válido")
